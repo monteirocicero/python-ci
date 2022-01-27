9c43565f10b9b18995a4e4fd9b1e126ba72981a4
Cicero Monteiro <cicerolmonteiro@gmail.com>
2022-01-27 19:00:09 -0300

    Fix issue with subtract method

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,8 +7,8 @@ class MyTestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(4, add(2, 2))
 
-    def test_add(self):
-        self.assertEqual(4, subtract(3, 5))
+    def test_subtract(self):
+        self.assertEqual(4, subtract(9, 5))
 
 
 if __name__ == '__main__':
